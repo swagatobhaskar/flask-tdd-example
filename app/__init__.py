36160be334a92549074ef92148e9069f7f4c4487
swagatobhaskar <bhaskarswagato@gmail.com>
2025-01-19 01:32:04 +0530

    Add: SQLAlchemy boilerplate, config file.

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,29 @@
 from flask import Flask
+from flask_migrate import Migrate
+from os import environ, getenv
 
+from .extensions import db
 from .routes import main_bp
+
+from config import DevConfig, ProdConfig
+
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
 
-    app.register_blueprint(main_bp)
+    env_type = getenv('ENV')
+    print("ENV-->", env_type)
+    
+    if env_type == 'development':
+        config = DevConfig
+    else:
+        config = ProdConfig
+
+    app.config.from_object(config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    app.register_blueprint(main_bp, url_prefix="/")
 
     return app
---
+++ app/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy.sql import func
+
+from .extensions import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True, blank=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now(), nullable=False)
+    
+    def __repr__(self):
+        return f"<User(id={self.id}, username={self.username}, email={self.email})"
